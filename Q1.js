44b73f8bfddea0166e9eb05e72c2ba41751e3da4
willy0077 <willy633526@gmail.com>
2019-05-04 15:01:09 +0800

    Q1(overtime)

--- Q1.js
+++ Q1.js
@@ -4,10 +4,12 @@ function reversechar(string) {
 
 var str = "abcdef";
 var str2 = "abcdef cow apple";
-
+var strtemp = "";
 
 for (let i = 0; i < 3; i++) {
-    console.log(reversechar(str2.split(" ")[i]));
-    str2 = str2.concat(reversechar(str2.split(" ")[i]));
+    console.log(typeof (reversechar(str2.split(" ")[i])));
+    // str2 = str2.push(reversechar(str2.split(" ")[i]));
+    strtemp += reversechar(str2.split(" ")[i]) + " ";
 }
+str2 = strtemp;
 console.log(str2);
